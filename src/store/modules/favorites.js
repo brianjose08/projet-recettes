f1824955648c6c42dd5cb05f16fa6da5cb6ae87b
Franco <e1794013@cmaisonneuve.qc.ca>
2021-02-25 08:58:56 -0500

    Favorite Module

---
+++ src/store/modules/favorites.js
@@ -0,0 +1,35 @@
+const getters = {
+  allFavoritesRecettes: (state) => state.listFavoritesRecettes,
+};
+
+const actions = {
+  fetchFavoritesRecettes({ commit }, data) {
+    commit('setFavoritesRecettes', data);
+  },
+  addFavoriteRecette({ commit }, data) {
+    commit('newFavoriteRecette', data);
+  },
+  deleteFavoriteRecette({ commit }, favoriteRecetteId) {
+    commit('removeFavoriteRecette', favoriteRecetteId);
+  },
+};
+
+const mutations = {
+  setFavoritesRecettes: (state, favoritesRecettes) => { (state.listFavoritesRecettes = favoritesRecettes); },
+  newFavoriteRecette: (state, favoriteRecette) => state.listFavoritesRecettes.unshift(favoriteRecette),
+  removeFavoriteRecette: (state, favoriteRecetteId) => {
+    (state.listFavoritesRecettes = state.listFavoritesRecettes
+      .filter((recette) => recette.id !== favoriteRecetteId));
+  },
+};
+
+const state = {
+  listFavoritesRecettes: [],
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
