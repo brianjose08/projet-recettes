2ba3c92ae9969da80494847663175cc0c879715b
Franco <e1794013@cmaisonneuve.qc.ca>
2021-03-10 00:40:50 -0500

    Calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="contenu">
-        <h1>Calendar</h1>
-    </div>
+  <div class="contenu">
+      <div id="calendar">
+      <section>
+        <div>March</div>
+        <div class="weekdays">
+          <div class="weekday" v-for="(weekday, index) in weekdays" :key="index">
+            {{ weekday }}
+          </div>
+        </div>
+        <div class="date">
+          <div class="day" v-for="(n, index) in 28" :key="index">
+            {{ n }}
+          </div>
+          <div class="day-hidden" v-if="month === February">29</div>
+          <div class="day-hidden" v-if="month !== February">30</div>
+          <div class="day-hidden" v-if="month !== February">31</div>
+        </div>
+      </section>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +27,8 @@ export default {
   name: 'Calendar',
   data() {
     return {
+      month: 'March',
+      weekdays: ['Mo', 'Th', 'We', 'Th', 'Fr', 'Sa', 'Su'],
       user: JSON.parse(localStorage.getItem('userGet')),
     };
   },
@@ -17,7 +36,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.contenu{
-  padding-top: 100px;
+@mixin calendar-layout($property) {
+  display: grid;
+  grid-template-columns: repeat(7, minmax(50px, 1fr));
+  grid-gap: 10px;
+  padding: $property;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    color: black;
+    border-radius: 5px;
+  }
+}
+
+.contenu {
+  padding: 200px 0px 155px 0px;
+
+}
+
+#calendar {
+  margin: auto;
+  width: 750px;
+  height: 500px;
+  background-color: #efefef;
+  overflow: hidden;
+  font-size: 40px;
+  .weekdays {
+    @include calendar-layout(10px 20px 20px);
+    background-color: rgba(0,0,0,0.7);
+    border-bottom: 1px solid #fff;
+
+    div:nth-child(7n) {
+      color: red($color: #000000);
+    }
+  }
+
+  .date {
+    @include calendar-layout(40px 20px 40px);
+    background-color: rbga(0,0,0,0.5);
+  }
 }
 </style>
